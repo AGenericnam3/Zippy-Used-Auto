4df1617b006eabb58bbf76b1c35a5e083239be81
AGenericnam3 <144026611+AGenericnam3@users.noreply.github.com>
2024-03-21 17:22:28 -0500

    Delete Midterm/model/database.php
    old database file

--- Midterm/model/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Set up the server
-$dsn ="mysql:host=localhost; dbname = zippyusedautos";
-$username = 'root';
-$password = 'password1';
-
-// Connect to the first database
-$conn1 = mysqli_connect($host, $username, $password, "zippyusedautos");
