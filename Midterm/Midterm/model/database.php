04de21f79a41d7904012096a4f8c2c6ee63d11b8
AGenericnam3 <144026611+AGenericnam3@users.noreply.github.com>
2024-03-21 17:21:00 -0500

    Add files via upload

---
+++ Midterm/Midterm/model/database.php
@@ -0,0 +1,8 @@
+<?php
+//Set up the server
+$dsn ="s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname = jawsdb";
+$username = 'duw041h4qnssnusy';
+$password = 'rz17x5zje3lgc7nc';
+
+// Connect to the first database
+$conn1 = mysqli_connect($host, $username, $password, "zippyusedautos");
